5f9b11817ec29b504ed219b5c7f0c478a7fca6a5
mqcah <mqcha94@outlook.jp>
2020-06-20 22:31:05 +0900

    [finish]5-6

--- config/routes.rb
+++ config/routes.rb
@@ -12,4 +12,6 @@ resources :post_images, only: [:new, :create, :index, :show, :destroy] do
     resource :post_comments, only: [:create, :destroy]
 end
  resources :users, only: [:show]
+ resources :users, only: [:show, :edit]
+ resources :users, only: [:show, :edit, :update]
 end
