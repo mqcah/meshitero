1fd5db591cd8203bf821fa6fa048986c3164fa28
mqcah <mqcha94@outlook.jp>
2020-06-20 22:08:40 +0900

    [finish]5-4

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,4 @@
 #     https: false
 #   )
 # end
+Refile.secret_key = 'f8922a2602932c899ea86b5f82480a981898e35fac32950795568d1c5f89b370dfc602e7b399afb793c737831f37fdbab285783edbd68fff0f8ca97df9e0f3ee'
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,22 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_06_18_094114) do
+ActiveRecord::Schema.define(version: 2020_06_20_120248) do
+
+  create_table "favorites", force: :cascade do |t|
+    t.integer "user_id"
+    t.integer "post_image_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "post_comments", force: :cascade do |t|
+    t.text "comment"
+    t.integer "user_id"
+    t.integer "post_image_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "post_images", force: :cascade do |t|
     t.text "shop_name"
